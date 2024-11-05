e2321fa141aa00dbbd5843ddaf0861b5e4d3b101
DESKTOP-SP667UE\sieun <2100004146@nttu.edu.vn>
2024-11-05 13:09:20 +0700

    no message

--- front-end/src/Components/Header.js
+++ front-end/src/Components/Header.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FaCaretDown } from "react-icons/fa";
+import { useAuth } from '../ContextAPI/authContext';
 
 const Header = ({ theme, toggleTheme }) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const [isUserDropdownOpen, setIsUserDropdownOpen] = useState(false); // State for User Icon dropdown
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // Track login status
+  const [isUserDropdownOpen, setIsUserDropdownOpen] = useState(false);
+  const { user, logout } = useAuth();
   const navigate = useNavigate();
 
   const toggleDropdown = () => {
@@ -16,32 +17,20 @@ const Header = ({ theme, toggleTheme }) => {
     setIsUserDropdownOpen((prev) => !prev);
   };
 
-  // Mock login function that navigates to /login and sets the login state
-  const handleMockLogin = () => {
-    const mockUsername = "testuser";
-    const mockPassword = "123456";
-
-    // Simulate a successful login
-    if (mockUsername === "testuser" && mockPassword === "123456") {
-      setIsLoggedIn(true);
-      navigate("/"); // Navigate to the home page or another page after login
-      alert("Login successful!"); // Optional success message
-    } else {
-      alert("Invalid username or password"); // Optional error message
+  const handleLogout = async () => {
+    try {
+      await logout();
+      setIsUserDropdownOpen(false);
+      navigate('/');
+    } catch (error) {
+      console.error('Logout error:', error);
     }
-  };
-
-  // Logout function
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-    setIsUserDropdownOpen(false); // Close the dropdown after logout
-    alert("Logged out successfully!");
   };
 
   return (
     <header
-      className={`${theme === "dark" ? "bg-gray-900 text-white" : "bg-white text-black"
-        } w-full shadow-md relative z-10`}
+      className={`${theme === "dark" ? "bg-gray-900 text-white" : "bg-white text-black"} 
+        w-full shadow-md relative z-10`}
     >
       <div className="max-w-[96rem] mx-auto flex justify-between items-center p-4">
         <Link to="/">
@@ -60,8 +49,8 @@ const Header = ({ theme, toggleTheme }) => {
             <li>
               <Link
                 to="/exercises"
-                className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"
-                  } hover:text-blue-500`}
+                className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"} 
+                  hover:text-blue-500`}
               >
                 All exercises
               </Link>
@@ -69,8 +58,8 @@ const Header = ({ theme, toggleTheme }) => {
             <li>
               <Link
                 to="/top-users"
-                className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"
-                  } hover:text-blue-500`}
+                className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"} 
+                  hover:text-blue-500`}
               >
                 Top users
               </Link>
@@ -78,35 +67,34 @@ const Header = ({ theme, toggleTheme }) => {
             <li className="relative">
               <button
                 onClick={toggleDropdown}
-                className={`flex gap-2 items-center ${theme === "dark" ? "text-gray-300" : "text-gray-600"
-                  } hover:text-blue-500`}
+                className={`flex gap-2 items-center ${
+                  theme === "dark" ? "text-gray-300" : "text-gray-600"
+                } hover:text-blue-500`}
               >
                 Video lessons
                 <FaCaretDown />
               </button>
               {isDropdownOpen && (
                 <div
-                  className={`absolute w-max left-0 mt-2 py-1 border border-gray-500 shadow-lg rounded-md ${theme === "dark"
-                    ? "bg-gray-900 text-gray-300"
-                    : "bg-white text-black"
-                    }`}
+                  className={`absolute w-max left-0 mt-2 py-1 border border-gray-500 
+                    shadow-lg rounded-md ${
+                    theme === "dark" ? "bg-gray-900 text-gray-300" : "bg-white text-black"
+                  }`}
                 >
                   <Link
                     to="/english-expressions"
-                    className={`block px-4 py-2 ${theme === "dark"
-                      ? "hover:bg-gray-700"
-                      : "hover:bg-gray-100"
-                      }`}
+                    className={`block px-4 py-2 ${
+                      theme === "dark" ? "hover:bg-gray-700" : "hover:bg-gray-100"
+                    }`}
                     onClick={() => setIsDropdownOpen(false)}
                   >
                     English expressions
                   </Link>
                   <Link
                     to="/english-pronunciation"
-                    className={`block px-4 py-2 ${theme === "dark"
-                      ? "hover:bg-gray-700"
-                      : "hover:bg-gray-100"
-                      }`}
+                    className={`block px-4 py-2 ${
+                      theme === "dark" ? "hover:bg-gray-700" : "hover:bg-gray-100"
+                    }`}
                     onClick={() => setIsDropdownOpen(false)}
                   >
                     English pronunciation
@@ -117,8 +105,8 @@ const Header = ({ theme, toggleTheme }) => {
             <li>
               <Link
                 to="/learntogether"
-                className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"
-                  } hover:text-blue-500`}
+                className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"} 
+                  hover:text-blue-500`}
               >
                 Learn Together
               </Link>
@@ -126,10 +114,10 @@ const Header = ({ theme, toggleTheme }) => {
             <li>
               <Link
                 to="/support-dailydictation"
-                className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"
-                  } flex items-center`}
+                className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"} 
+                  flex items-center`}
               >
-                Help{" "}
+                Help me{" "}
                 <span role="img" aria-label="praying hands" className="ml-1">
                   🙏
                 </span>
@@ -138,22 +126,18 @@ const Header = ({ theme, toggleTheme }) => {
           </ul>
 
           <ul className="flex items-center space-x-4 ml-6">
-            {isLoggedIn ? (
-              // Show the logged-in interface
+            {user ? (
               <>
                 <li>
-                  <span
-                    className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"
-                      }`}
-                  >
+                  <span className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"}`}>
                     0 minutes
                   </span>
                 </li>
                 <li>
                   <Link
-                    to="/notes"
-                    className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"
-                      }`}
+                    to="/in-progress"
+                    className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"} 
+                      hover:text-blue-500`}
                   >
                     In-progress
                   </Link>
@@ -161,8 +145,8 @@ const Header = ({ theme, toggleTheme }) => {
                 <li>
                   <Link
                     to="/notes"
-                    className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"
-                      }`}
+                    className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"} 
+                      hover:text-blue-500`}
                   >
                     Notes
                   </Link>
@@ -170,44 +154,41 @@ const Header = ({ theme, toggleTheme }) => {
                 <li className="relative">
                   <button
                     onClick={toggleUserDropdown}
-                    className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"
-                      } hover:text-blue-500`}
+                    className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"} 
+                      hover:text-blue-500`}
                   >
-                    User Icon
+                    {user.username || "User"} 
                   </button>
                   {isUserDropdownOpen && (
                     <div
-                      className={`absolute right-0 mt-2 py-1 w-48 border border-gray-500 shadow-lg rounded-md ${theme === "dark"
-                        ? "bg-gray-900 text-gray-300"
-                        : "bg-white text-black"
-                        }`}
+                      className={`absolute right-0 mt-2 py-1 w-48 border border-gray-500 
+                        shadow-lg rounded-md ${
+                        theme === "dark" ? "bg-gray-900 text-gray-300" : "bg-white text-black"
+                      }`}
                     >
                       <Link
                         to="/profile"
-                        className={`block px-4 py-2 ${theme === "dark"
-                          ? "hover:bg-gray-700"
-                          : "hover:bg-gray-100"
-                          }`}
+                        className={`block px-4 py-2 ${
+                          theme === "dark" ? "hover:bg-gray-700" : "hover:bg-gray-100"
+                        }`}
                         onClick={() => setIsUserDropdownOpen(false)}
                       >
                         Profile
                       </Link>
                       <Link
                         to="/settings"
-                        className={`block px-4 py-2 ${theme === "dark"
-                          ? "hover:bg-gray-700"
-                          : "hover:bg-gray-100"
-                          }`}
+                        className={`block px-4 py-2 ${
+                          theme === "dark" ? "hover:bg-gray-700" : "hover:bg-gray-100"
+                        }`}
                         onClick={() => setIsUserDropdownOpen(false)}
                       >
                         Settings
                       </Link>
                       <button
                         onClick={handleLogout}
-                        className={`block px-4 py-2 text-left w-full ${theme === "dark"
-                          ? "hover:bg-gray-700"
-                          : "hover:bg-gray-100"
-                          }`}
+                        className={`block px-4 py-2 text-left w-full ${
+                          theme === "dark" ? "hover:bg-gray-700" : "hover:bg-gray-100"
+                        }`}
                       >
                         Logout
                       </button>
@@ -216,14 +197,12 @@ const Header = ({ theme, toggleTheme }) => {
                 </li>
               </>
             ) : (
-              // Show Login/Register links when not logged in
               <>
                 <li>
                   <Link
                     to="/login"
-                    onClick={handleMockLogin}
-                    className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"
-                      } hover:text-blue-500`}
+                    className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"} 
+                      hover:text-blue-500`}
                   >
                     Login
                   </Link>
@@ -231,8 +210,8 @@ const Header = ({ theme, toggleTheme }) => {
                 <li>
                   <Link
                     to="/register"
-                    className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"
-                      } hover:text-blue-500`}
+                    className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"} 
+                      hover:text-blue-500`}
                   >
                     Register
                   </Link>
@@ -242,8 +221,8 @@ const Header = ({ theme, toggleTheme }) => {
             <li>
               <button
                 onClick={toggleTheme}
-                className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"
-                  } hover:text-blue-500 text-2xl`}
+                className={`${theme === "dark" ? "text-gray-300" : "text-gray-600"} 
+                  hover:text-blue-500 text-2xl`}
               >
                 {theme === "dark" ? "🌚" : "🌞"}
               </button>
